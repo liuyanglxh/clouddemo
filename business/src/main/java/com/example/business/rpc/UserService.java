a4b5c6211f6e3669a114176f3963e0c63a8e0f41
liuyang <yang.liu@mail.dealmoon.com>
2019-04-04 14:25:37 +0800

    1

--- business/src/main/java/com/example/business/rpc/UserService.java
+++ business/src/main/java/com/example/business/rpc/UserService.java
@@ -2,8 +2,7 @@ package com.example.business.rpc;
 
 import com.example.business.rpc.fallback.UserServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
@@ -11,6 +10,6 @@ import java.util.Map;
 @FeignClient(name = "user-center", path = "api/user/v1", fallbackFactory = UserServiceFallback.class)
 public interface UserService {
 
-    @RequestMapping(path = "info", method = RequestMethod.GET)
+    @GetMapping(path = "info")
     Map<String, Object> getUserInfo(@RequestParam("userId") Integer userId);
 }
